12d97ffd4bafcb7e0e0fb0d1531a3d63da02e988
coderangel117 <perinogabriel1@gmail.com>
2024-12-25 15:18:14 +0100

    feat [auth] : wip - First add module - Add passport with npm

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, TaskModule],
+  imports: [UserModule, TaskModule, AuthModule],
   controllers: [],
   providers: [PrismaService],
 })
